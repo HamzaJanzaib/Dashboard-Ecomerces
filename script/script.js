742874be72bace7e765d30707c6ca1efa8b819fd
hamza janzaib <hbahi024@gmail.com>
2025-02-05 03:23:29 -0800

    ADDING MANAGE PRODUCTS PAGE

--- script/script.js
+++ script/script.js
@@ -1,1 +1,57 @@
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Sindh', 'Punjab', 'Balochistan', 'KPK', 'FATA', 'Other'],
+        datasets: [{
+            label: 'Our Sales',
+            data: [2, 12, 3, 9, 2, 3],
+            borderWidth: 1,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)', 
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)', 
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ]
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+// Conversion Chart
+const conversionChart = new Chart(document.getElementById('conversionChart'), {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [65.2, 34.8],
+            backgroundColor: ['#ff6b6b', '#282E33'],
+            borderWidth: 0
+        }]
+    },
+    options: {
+        cutout: '75%',
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    }
+});
 
